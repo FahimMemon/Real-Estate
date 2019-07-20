87d0b32c490bae8f951c0b86316ce7cf069870a2
FahimMemon <fahimraza089@gmail.com>
2019-07-20 12:36:21 +0500

    Cards made

--- src/Containers/Delete Property/Delete.js
+++ src/Containers/Delete Property/Delete.js
@@ -99,7 +99,7 @@ class Delete extends Component {
                                     </div>
                                     <h1>Tailored Jeans</h1>
                                     <p class="price">$19.99</p>
-                                    <p>Some text about the jeans. Super slim and comfy lorem ipsum lorem jeansum. Lorem jeamsun denim lorem jeansum.</p>
+                                    <p>{f.detail}</p>
                                     <p><button>Add to Cart</button></p>
                                 </div>
                             })
